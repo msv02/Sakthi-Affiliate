9ca57fdfc71f36bd309c3beba148bf293a42c9b4
msv02 <editingstudiooffical@gmail.com>
2026-06-17 06:56:32 +0530

    tenth commit

--- script.js
+++ script.js
@@ -32,105 +32,105 @@ const products = [
     description: "Stretchable & adjustable eye gel mask with soft velvet backing for dark circles, puffiness, and stress relief.",
     price: "₹199",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/q_auto/f_auto/v1781197038/81FrL0enhnL_ygm5le.jpg",
-    link: "https://www.amazon.in/BoldHealth-Cooling-Circles-Stretchable-Relaxing/dp/B0B4WTMRSS?crid=3SO7IIL1E87DJ&dib=eyJ2IjoiMSJ9.SMHcs6JDHAoxnSE0FAVLk-mwsFVaE0myJINpOVjhaIQoW5a_wRGJPx_og2hqEhEILwJ7SggBfjjmreKu3nJ_Cv3btcmwZ0lkcPipS8U-Ic89WTPmvxcEHkcn7MQkIEJNFf9MpeoOItcGOW4P0ytq607ZaSAbbXWTfj00w1mAawldFZXKbVUkP2uotmWLen7iB8mBL2xCZwmYigaWjJ8Uky5ofliJzP4txCAtOQrtHSs_dG31azisfpOaWhZx_4gB8SsszPxy1hhv9Iu67_H13AzpN85kNPQVMUwSxGL5Wic.VSJlYZdjaO7UlaxTpFsiLifPgyeYjddy1fJQjpu18mA&dib_tag=se&keywords=eye%2Bmask%2Bwith%2Bgel%2Btype&qid=1781196722&sprefix=%2Caps%2C529&sr=8-1-spons&aref=mXcnixCA6Y&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1&linkCode=ll2&tag=msv026-21&linkId=24cd83905e28ffb822efe1b54126f2fa&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B0BNDSWLL4?smid=A15APWRK6P7LBV&th=1&linkCode=ll2&tag=msv02602-21&linkId=322f31ad904d84d3e64b688be99b3aa0&ref_=as_li_ss_tl"
   },
   {
     id: "deconstruct-gel-sunscreen",
     name: "Deconstruct Face Gel Sunscreen SPF 55+",
     description: "A photostable, lightweight, and non-greasy gel sunscreen that protects combination-to-oily skin without leaving a white cast.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/q_auto/f_auto/v1781199620/deconstruct-lightweight-gel-sunscreen-spf-55-and-pa-50-gm_1_display_1772096223_eb987728.jpg_shggze.webp",
-    link: "https://www.amazon.in/Deconstruct-Photostable-Lightweight-Non-greasy-combination/dp/B0B45RB1RV?crid=AK0P23UB6Z2Q&dib=eyJ2IjoiMSJ9.dNZSHit4IDLj1ubqNuTXPg2OKKk9eV5J0Scc_upaB_P2md6gCgpvriEjIBPDZj0Jr273tduuTXE_Z7Rjcll2h_2cm2YpMhu_qz0dKyLoYmwRTJoAzOtL9-VPhucbK0Zi7bMwoN5b_X6oX3dgH-zedVFDBJEACdrzmja0Z7vx3X5urEsMWnK2ataH9N90tUVwQZ-_01YMziK_V2Z-NJvXhKIHvOBOp1AZRzYGh-6UCSdTLHBsV7-ERUFWJdfM6unaOVNi_cYAl_mZVyRKHBjeoyp33PboGjmmt42lZ3vPi0I.yxarrf3nLgtw-EPh22Hl19dXUOpVVIYZo2uiFm2TW9g&dib_tag=se&keywords=sunscreen&qid=1781199533&sprefix=sunscreen%2Caps%2C401&sr=8-5&th=1&linkCode=ll2&tag=msv026-21&linkId=0f6907e49f194ca90a8399c398365717&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B0B45RB1RV?smid=AINB595VN21QW&th=1&linkCode=ll2&tag=msv02602-21&linkId=b0452821de8a3888876a0d6ca8dfb2c6&ref_=as_li_ss_tl"
   },
   {
     id: "reen-naturals-beetroot-soap",
     name: "Reen Naturals Beetroot Brightening Soap",
     description: "Handcrafted soap enriched with beetroot extracts and essential oils to gently cleanse and brighten your skin naturally.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/q_auto/f_auto/v1781200579/Untitled_design_6.png_lfm3gp.webp",
-    link: "https://www.amazon.in/Naturals-Brightening-Beetroot-Extracts-Essential/dp/B0DT99VHKW?crid=1NA6ZRG8RUSDB&dib=eyJ2IjoiMSJ9.6ZT3nnH5PsW_17J2n7ASqEapyl7CMt_-hFx48OilP5WZOm6tMOvPbAiaX1EVr_I0nH9M0aC-om7NUtR3KqtJGDxBpHusNtteNeHB-BdYtFiMn2mvYAsZyju24xd-mNVBFma5yW0fF9200IHidkBR01kojLt0WH_AaDU3-ID1DKDea4lTZldoTQ3d3F2aqyrQyoy0xzzfsMHs_Mdfy0A_4gN_6S44JyjIj2ogh5JxlL4kCBdfpPvc6CVsW6pC8B-Idy2_B853RVwfV_nbuBO_-JNxiCMpHs39nhEh2TpfZS4.1ldhhUKK9asiRUpTddAkVg8wdWfZbvJU6y9e1kiVR4c&dib_tag=se&keywords=reen%2Bnaturals%2Bsoap&qid=1781198769&sprefix=reen%2B%2Caps%2C797&sr=8-5&th=1&linkCode=ll2&tag=msv026-21&linkId=e08c0d966a5fdb0f908e03269487f574&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B0DT99VHKW?smid=A8HJQWCREB2SL&th=1&linkCode=ll2&tag=msv02602-21&linkId=c06848b7fc819a9f4d46ac573c86c926&ref_=as_li_ss_tl"
   },
   {
     id: "klm-nevlon-moisturizer",
     name: "KLM Nevlon Intense Moisturizing Cream",
     description: "Formulated for extended hydration and deep skin nourishment, suitable for dry and sensitive skin.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/q_auto/f_auto/v1781200618/ChatGPT_Image_Jun_11_2026_at_11_26_32_PM_rqi1ic.png",
-    link: "https://www.amazon.in/KLM-Moisturizing-Extended-Hydration-Suitable/dp/B0D3Z7QQW8?crid=EC0A5JB86G16&dib=eyJ2IjoiMSJ9.5x_9-2w5nIRiR2KashaxlavjHnj9bx0vELTIU25Y22fngoCfkoggUlxgXq3MLBZzh3yTFn8odyJBqO_wEoz6YaMsKpIPoImvgOS77VmKjXeSgIUB_CiZ-UtxuGmDbFFVn6RiVO2MEE73rHnsg8g-ACaNGnrsvKeG4lLQqR65OojtzJhPVmnW3xlVsyRPMit_ykRGkxeaISq-v4Hlk3spvyJyTU351rOJmOrZcvZfAhE.Pne6WBCj5AOhLqR5FnChOD5MJofa5u0CTUkrmaFvwNU&dib_tag=se&keywords=nevlon+intense&qid=1781200166&sprefix=nevlon+inten%2Caps%2C527&sr=8-2&linkCode=ll2&tag=msv026-21&linkId=d66d3238a8957d4d7283cf59fafc2080&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B0D3Z7QQW8?smid=A1Y19I0OE4KHXF&psc=1&linkCode=ll2&tag=msv02602-21&linkId=7615d637924114ec5a2b7839281c2768&ref_=as_li_ss_tl"
   },
   {
     id: "diamond-color-contact-lenses",
     name: "Diamond Monthly Color Contact Lenses",
     description: "Premium monthly color contact lenses that provide exceptional comfort and a natural, vibrant eye enhancement.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/q_auto/f_auto/v1781273477/ChatGPT_Image_Jun_12_2026_at_07_40_08_PM_pxsxei.png",
-    link: "https://www.amazon.in/Diamond-Monthly-Color-Contact-Lenses/dp/B0837QW56H?crid=2HJUEXIEBYTK7&dib=eyJ2IjoiMSJ9.5cMK0qQHSAUCCdHkJZ-93yqVd9_kmz97nOqMsxd9h4TGS_cvsY3b6ZPW6oQSyn__DOZ8UqaNJnccAhUzMO_hCUQbknyAqkD6C1bXcvixjRVdQlAfFf12YWTqgxFuhjO5yZWU1SKAHe_d4YjnZ7CqNzkVQS_JePyVZmS4RQQBLtbjAl61IH2dzX5mdBvEQ8FgRVqR4tH9BH-wW-ZH8OXR0NbHHmm4vaIsRweUk24RIU-g1V5mZKcaQDmBITerQi2ZcsOB9V_-qeUxeVWJTDZKL7f1WkwtSETUAGr6tM2iAB4.RDKDVfi-477drJyHbMhcqJIGOFYJ8F3rIBLAgj76Qc4&dib_tag=se&keywords=blue+monthly+contact+lens+zero+power&qid=1781272580&sprefix=blue+monthly+contact+lens+zero+power%2Caps%2C621&sr=8-9&linkCode=ll2&tag=msv026-21&linkId=534d9dc2b32a0db2ad4cc092e4b9adc5&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B0837QW56H?smid=A12DTCKM3B4RXS&psc=1&linkCode=ll2&tag=msv02602-21&linkId=d55833a06060cbcb91aee7b8a052fb37&ref_=as_li_ss_tl"
   },
   {
     id: "spartan-shield-dio-speedometer",
     name: "Spartan Shield DIO 110/125 Screen Protector",
     description: "Premium anti-scratch, bubble-free screen guard film designed to protect the digital instrument console of your Honda Dio scooter.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/q_auto/f_auto/v1781284662/ChatGPT_Image_Jun_12_2026_at_10_47_18_PM_iuvpkj.png",
-    link: "https://www.amazon.in/Spartan-Shield-DIO-110-Speedometer/dp/B0FPXP9HJY?crid=3IZIBMVVWMG0X&dib=eyJ2IjoiMSJ9.b_3pzk_VONC0WfADNwh8KXZKtUwuyOjBYy_WVU28kS0GkQ2Q_9kaKYIP875RfwZl8YaE0i7t0Do10_Ugzc_LAFe6z10KKnxwpBbY_rG-TzhxehMsAh3E7m8JuJ93cQ5K.B1-mCUIZNeeg6gZ87FOnSPPBMIpjKzlWcIFxdoJgDGA&dib_tag=se&keywords=dio%2B125%2Bh-smart%2Bdisplay%2Btemper&qid=1781283090&refinements=p_72%3A1318476031%2Cp_n_feature_fourteen_browse-bin%3A11142592031&rnid=11142591031&sprefix=dio%2B125%2Bh-smart%2Bdisplay%2Btemper%2Caps%2C445&sr=8-1&th=1&linkCode=ll2&tag=msv026-21&linkId=2c52f95d8b8adcd63e89fd7bb9f47c20&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B0FPXP9HJY?smid=A2EYRGDXQ2P1HM&th=1&linkCode=ll2&tag=msv02602-21&linkId=2b170a7d532dd286fa1f910feaf17c36&ref_=as_li_ss_tl"
   },
   {
     id: "reen-charcoal-multani-mitti",
     name: "Reen Naturals Charcoal & Multani Mitti Powder Combo",
     description: "A deep pore cleansing face pack combo featuring Activated Bamboo Charcoal and Multani Mitti powders to detoxify, exfoliate, and brighten skin naturally.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/q_auto/f_auto/v1781449080/ChatGPT_Image_Jun_14_2026_at_08_27_28_PM_jqmgee.png",
-    link: "https://www.amazon.in/gp/product/B0CXXNFHLC?smid=A8HJQWCREB2SL&psc=1&linkCode=ll2&tag=msv026-21&linkId=8522e85ad6faef842c5d8801b6a3252a&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B0CXXNFHLC?smid=A8HJQWCREB2SL&psc=1&linkCode=ll2&tag=msv02602-21&linkId=da8d407e2bb1326a2afe0abeb35dd0ff&ref_=as_li_ss_tl"
   },
   {
     id: "reen-naturals-hair-dye-combo",
     name: "Reen Naturals Henna & Indigo Powder Combo",
     description: "100% natural and organic Henna and Indigo powders for safe, chemical-free hair coloring and deep conditioning.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/q_auto/f_auto/v1781449079/ChatGPT_Image_Jun_14_2026_at_08_25_45_PM_fany1q.png",
-    link: "https://www.amazon.in/gp/product/B0GYD1QL7G?smid=AXOGFIT0PZZ7G&th=1&linkCode=ll2&tag=msv026-21&linkId=c14829f4e264041cf08c5dbbb268ab40&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B0GYD1QL7G?smid=AXOGFIT0PZZ7G&th=1&linkCode=ll2&tag=msv02602-21&linkId=06f2d4a981b21982f207c1d48c9be7ec&ref_=as_li_ss_tl"
   },
   {
     id: "portronics-mport-30-card-reader",
     name: "Portronics Mport 30 Plus Card Reader",
     description: "A multifunctional, high-speed plug-and-play card reader with built-in storage slots for SD/microSD cards, SIM cards, and a SIM ejector pin.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/q_auto/f_auto/v1781449602/61SsgxQEULL._AC_UF1000_1000_QL80__gj4y5v.jpg",
-    link: "https://www.amazon.in/gp/product/B0DK1KSNF4?smid=AJ6SIZC8YQDZX&psc=1&linkCode=ll2&tag=msv026-21&linkId=0f00e1d37902e9cb1b15a8a478f738dd&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B0DK1KSNF4?smid=AJ6SIZC8YQDZX&psc=1&linkCode=ll2&tag=msv02602-21&linkId=ce011ce87c51ea7fc930f1b570b07a6b&ref_=as_li_ss_tl"
   },
   {
     id: "portronics-tornado-air-duster",
     name: "Portronics Tornado Handheld Air Duster",
     description: "A powerful, rechargeable, 130,000 RPM high-speed cordless air duster with three speed modes to easily clean keyboards, laptops, CPUs, and car interiors.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/q_auto/f_auto/v1781449601/61_N9FyfSzL._AC_UF1000_1000_QL80__ux5yfv.jpg",
-    link: "https://www.amazon.in/gp/product/B0F9LKX5XG?smid=A2AL6IVND0I91F&th=1&linkCode=ll2&tag=msv026-21&linkId=3deefa9cd0ae4df982cd5c9fbcdb7b94&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B0F9LKX5XG?smid=A2AL6IVND0I91F&th=1&linkCode=ll2&tag=msv02602-21&linkId=28940ad56091e1ce43f0687e55091379&ref_=as_li_ss_tl"
   },
   {
     id: "amazon-echo-dot-5th-gen",
     name: "Amazon Echo Dot (5th Gen) Smart Speaker",
     description: "Smart speaker with Alexa, offering deeper bass, vibrant sound, built-in temperature sensor, and motion detection.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/q_auto/f_auto/v1781449603/Amazon-Echo-Dot-5th-Gen-Smart-speaker-with-vibrant-sound-Motion-Detection-Temper-0-1200x1200_tikntz.jpg",
-    link: "https://www.amazon.in/gp/product/B09B8XJDW5?smid=AJ6SIZC8YQDZX&th=1&linkCode=ll2&tag=msv026-21&linkId=3fabce184d9de5f23e076495960e0da6&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B09B8XJDW5?smid=AJ6SIZC8YQDZX&th=1&linkCode=ll2&tag=msv02602-21&linkId=148c60959bce8417de89bde63701335b&ref_=as_li_ss_tl"
   },
   {
     id: "squeeze-rinsing-eye-cup",
     name: "Squeeze Rinsing Eye Wash Cup",
     description: "An ergonomic silicone eye wash cup designed to gently rinse away dust, makeup residue, and irritants using manual air pressure. Perfect for refreshing tired and irritated eyes.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/v1781488451/51H4_5GAVIL._AC_UF1000_1000_QL80__reoijb.jpg",
-    link: "https://www.amazon.in/gp/product/B0DVLKJVYP?smid=A38NIY4R9X6VUN&th=1&linkCode=ll2&tag=msv026-21&linkId=f5f0201760d1db4be5a8228381122344&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B0DVLKJVYP?smid=A38NIY4R9X6VUN&th=1&linkCode=ll2&tag=msv02602-21&linkId=c5cfef354fcc0e4cc770fa03afecb668&ref_=as_li_ss_tl"
   },
   {
     id: "glassverse-vivo-s1-pro-tempered-glass",
     name: "GlassVerse 9H Tempered Glass for Vivo S1 Pro",
     description: "Premium 9H surface hardness tempered glass screen protector for Vivo S1 Pro (6.38-inch). Features anti-glare, scratch resistance, and rounded edges with a free installation kit.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/v1781534421/ChatGPT_Image_Jun_15_2026_at_08_09_53_PM_lc1ojj.png",
-    link: "https://www.amazon.in/gp/product/B0BPS8PFKH?smid=ANVCTQ7K82P8U&th=1&linkCode=ll2&tag=msv026-21&linkId=1f7850effd3cb7fc778c00dc670c1919&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B0BPS8PFKH?smid=ANVCTQ7K82P8U&th=1&linkCode=ll2&tag=msv02602-21&linkId=623c79cb89b6893e5846ab734a5a2352&ref_=as_li_ss_tl"
   },
   {
     id: "supcares-vivo-y200e-tempered-glass",
     name: "SupCares Tempered Glass for Vivo Y200e",
     description: "High-quality 9H edge-to-edge tempered glass screen protector for Vivo Y200e (6.67-inch). Includes an easy-to-use installation kit for bubble-free application.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/v1781534589/ChatGPT_Image_Jun_15_2026_at_08_12_50_PM_syyy46.png",
-    link: "https://www.amazon.in/gp/product/B0D1QS88T6?smid=AU2QX2WP7B51I&th=1&linkCode=ll2&tag=msv026-21&linkId=c8bbdc5cb7abec9bb7754c3a1e9a37f9&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/gp/product/B0D1QS88T6?smid=AU2QX2WP7B51I&th=1&linkCode=ll2&tag=msv02602-21&linkId=914665d3aaba22eb9c71bcf88ad75c45&ref_=as_li_ss_tl"
   },
   {
     id: "popio-samsung-a06-tempered-glass",
     name: "POPIO Gorilla Tempered Glass for Samsung Galaxy A06",
     description: "Military-grade 9H hardness Gorilla tempered glass screen protector for Samsung Galaxy A06 / F06 / M06. Features edge-to-edge protection, scratch resistance, and bubble-free installation.",
     image: "https://res.cloudinary.com/dbqd8ehhf/image/upload/v1781537974/ChatGPT_Image_Jun_15_2026_at_09_09_12_PM_er5liy.png",
-    link: "https://www.amazon.in/POPIO-Samsung-Galaxy-A06-M06/dp/B0DP26T4XY?crid=1B167GJGEN25S&dib=eyJ2IjoiMSJ9.90lyWHHHRX9rNVk6WSFSqyY_kYqS2y_NQ3Ue6PjUa3pJim0XnPxTjtVONjaflmooYdiY9X0vQkhAMSl33FPZgUPse7J6iWok41-BFRRf52NwQ8ysdHYN_fVIWnWr5TKWyX7DP3rttEULYE4RfFVT2eNrDDmCVUvHssO0VN6kWeC8D-1JOaa_Rx_U6c5WRpOCdWcyTdA9Vfvpns1vQ7pHgIsmNGIeDUGxp68EFkkDpVs.FoQHBDacd4RlTOhP4GnR1zOdjA9HhZki02bblGG93Hg&dib_tag=se&keywords=samsung%2Ba06%2Btempered%2Bglass&qid=1781537735&rnid=1318475031&sprefix=samsung%2Ba06%2Btempered%2Caps%2C411&sr=8-2-spons&aref=9cC2ibYkkf&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1&linkCode=ll2&tag=msv026-21&linkId=f899deaf5f921e1eedadc282dece410a&ref_=as_li_ss_tl"
+    link: "https://www.amazon.in/POPIO-Samsung-Galaxy-A06-M06/dp/B0DP26T4XY?crid=1CDM75U8WXZ2W&dib=eyJ2IjoiMSJ9.90lyWHHHRX9rNVk6WSFSq_sddmfAFXZxAXyb_hLCDdrC3mF4hTOk6U9neVNqxbMszhXIB7UODqzCf5y5GHy5I9bNmoxgE94ICZkXN8FbIpNhLHPJrPxujPzuito_N7vM7EMI29zKRaeGjWbWF8VhkWYDbeUGZz8h1hbwREiFIKNYJB5bgsTSM3MsHUlPn4mTfIFWZM2zIgL97YUjF8BB7S7f7rGjHSuSryVAJa4rfTc.MJMcCAOD7II9oDkPpn_44pFUEaGTVqqpbycxcX5Mk1w&dib_tag=se&keywords=POPIO%2BMilitary-Grade%2BGorilla%2BTempered%2BGlass%2Bfor%2BSamsung%2BGalaxy%2BA06%2B%2F%2BF06%2B%2F%2BM06%2B%2F%2BA07%2B%2F%2BF07%2B%2F%2BM07%2B%7C%2BPack%2Bof%2B1%2B%7C%2B9H%2BHardness%2C%2BEdge-to-Edge%2C%2BBubble-Free&nsdOptOutParam=true&qid=1781659201&sprefix=popio%2Bmilitary-grade%2Bgorilla%2Btempered%2Bglass%2Bfor%2Bsamsung%2Bgalaxy%2Ba06%2B%2F%2Bf06%2B%2F%2Bm06%2B%2F%2Ba07%2B%2F%2Bf07%2B%2F%2Bm07%2Bpack%2Bof%2B1%2B9h%2Bhardness%2C%2Bedge-to-edge%2C%2Bbubble-free%2Caps%2C488&sr=8-3&th=1&linkCode=ll2&tag=msv02602-21&linkId=bdfc7e13f68b2ea17ae5c1eed8896138&ref_=as_li_ss_tl"
   }
 ];
 // ============ END OF PRODUCTS ============
